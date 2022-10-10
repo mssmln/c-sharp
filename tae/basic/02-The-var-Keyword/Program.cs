77c67e6f27ea9fa630edb028496f703fea8ce0f0
m.croci <m.croci@avanade.com>
2022-10-10 14:43:47 +0200

    user input and var

---
+++ tae/basic/02-The-var-Keyword/Program.cs
@@ -0,0 +1,17 @@
+﻿// A variable can be explicitly declared with its type before it is used.
+// Alternatively, C# provides a handy function to enable the compiler to determine the type of the variable automatically based on the expression it is assigned to.
+// Variables declared using the var keyword are called implicitly typed variables.
+// Implicitly typed variables must be initialized with a value.
+
+using System;
+
+namespace _02_The_var_Keyword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("do not overuse var!");
+        }
+    }
+}
---
+++ tae/basic/UserInput/Program.cs
@@ -0,0 +1,17 @@
+﻿// The Console.ReadLine() method returns a string value.
+// If you are expecting another type of value (such as int or double), the entered data must be converted to that type.
+// For integer conversion, there are three alternatives available based on the bit size of the integer: Convert.ToInt16, Convert.ToInt32 and Convert.ToInt64. The default int type in C# is 32-bit.
+
+using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are {0} years old", age);
+        }
+    }
+}
