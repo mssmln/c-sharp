89cf9c61d3600dad70dfbde45b7457bc3571ca80
m.croci <m.croci@avanade.com>
2022-12-09 15:53:42 +0100

    finished more on classes chapter

---
+++ tae/oop/12-Indexers/Program.cs
@@ -0,0 +1,55 @@
+﻿// An indexer allows objects to be indexed like an array.
+// As discussed earlier, a string variable is actually an object of the String class. Further, the String class is actually an array of Char objects. In this way, the string class implements an indexer so we can access any character (Char object) by its index
+// Arrays use integer indexes, but indexers can use any type of index, such as strings, characters, etc.
+
+// Declaration of an indexer is to some extent similar to a property. The difference is that indexer accessors require an index.
+// Like a property, you use get and set accessors for defining an indexer. However, where properties return or set a specific data member, indexers return or set a particular value from the object instance.
+// Indexers are defined with the this keyword.
+// You typically use an indexer if the class represents a list, collection, or array of objects.
+
+// The music app you enjoy allows you to choose 5 music genres to follow.
+// The program you are given takes 5 music genres as input and stores them in the MusicGenres object as an array.
+// Fix the program by declaring an indexer inside the MusicGenres class so that the given outputs work correctly.
+
+using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Now, when we declare an object of class MusicGenres, we use an index to refer to specific objects like the elements of an array
+            MusicGenres genres = new MusicGenres();
+
+            int count = 0;
+            while (count < 5)
+            {
+                genres[count] = Console.ReadLine();
+                count++;
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Following: " + genres[i]);
+            }
+        }
+    }
+    class MusicGenres
+    {
+        private string[] genres = new string[5];
+
+        // As you can see, the indexer definition includes the this keyword and an index, which is used to get and set the appropriate value.
+        public string this[int index]
+        {
+            get
+            {
+                return genres[index];
+            }
+            set
+            {
+                genres[index] = value;
+            }
+        }
+    }
+}
---
+++ tae/oop/13-Operator-Overloading/Program.cs
@@ -0,0 +1,45 @@
+﻿// Most operators in C# can be overloaded, meaning they can be redefined for custom actions.
+// For example, you can redefine the action of the plus (+) operator in a custom class.
+// Overloaded operators are methods with special names, where the keyword operator is followed by the symbol for the operator being defined.
+// Similar to any other method, an overloaded operator has a return type and a parameter list.
+// Additionally, the overloaded operator must be static.
+// All arithmetic and comparison operators can be overloaded. For instance, you could define greater than and less than operators for the boxes that would compare the Boxes and return a boolean result. Just keep in mind that when overloading the greater than operator, the less than operator should also be defined.
+
+// You and your friend are playing a game as one team. Each player must pass 2 rounds and gets points for each round passed.
+// The program you are given creates two Score objects where each round scores are stored (they are passed to a constructor).
+// Overload the + operator for the Score class to calculate the team score for every round.
+
+using System;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Score tm1 = new Score(2, 3);
+            Score tm2 = new Score(4, 2);
+            Score finalScores = tm1 + tm2;
+
+            Console.WriteLine("Round 1: " + finalScores.round1Score);
+            Console.WriteLine("Round 2: " + finalScores.round2Score);
+        }
+    }
+    class Score
+    {
+        public int round1Score { get; set; }
+        public int round2Score { get; set; }
+        public Score(int r1, int r2)
+        {
+            round1Score = r1;
+            round2Score = r2;
+        }
+        public static Score operator + (Score a, Score b)
+        {
+            int h = a.round1Score + b.round1Score;
+            int w = a.round2Score + b.round2Score;
+            Score res = new Score(h, w);
+            return res;
+        }
+    }
+}
